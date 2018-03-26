b56f9b53c6d3b139bac2fa6fb1dcbe7c53a0079c
Victor Seiji Hariki <victorseijih@gmail.com>
2018-03-26 17:39:01 -0300

    Fixed paths

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'HIPO-Interpreter/static',
+    assetsPublicPath: '/HIPO-Interpreter',
 
     /**
      * Source Maps
